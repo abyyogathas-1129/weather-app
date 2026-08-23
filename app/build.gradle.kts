4e07e5041c63aac172c644d0d17bab3cfa206f2c
abyyogathas-1129 <abyyogathas@gmail.com>
2026-08-23 12:32:37 +0530

    chore: update build config and internet permission

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,4 +42,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
 }
